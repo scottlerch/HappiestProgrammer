9ca1e3cf27e4b7b2b2c711326ec11ddb8268b703
Scott Lerch <scottlerch@gmail.com>
2013-08-04 22:20:41 -0700

    Use secondary source for word sentiments

--- HappiestProgrammer.Core/SentimentAnalysis/WordSentiments.cs
+++ HappiestProgrammer.Core/SentimentAnalysis/WordSentiments.cs
@@ -13,23 +13,8 @@
         {
             this.sentiments = new Dictionary<string, float>(StringComparer.InvariantCultureIgnoreCase);
 
-            var positiveWords =
-                File.ReadLines(@"Resources\positive-words.txt")
-                    .Where(s => !s.StartsWith(";") && !string.IsNullOrWhiteSpace(s));
-
-            foreach (var word in positiveWords)
-            {
-                this.AddSentiment(word, 1);
-            }
-
-            var negativeWords =
-                File.ReadLines(@"Resources\negative-words.txt")
-                    .Where(s => !s.StartsWith(";") && !string.IsNullOrWhiteSpace(s));
-
-            foreach (var word in negativeWords)
-            {
-                this.AddSentiment(word, -1);
-            }
+            this.LoadPosNegWords();
+            this.LoadAffin();
         }
 
         public float GetScore(string word)
@@ -42,6 +27,39 @@
             }
 
             return 0;
+        }
+
+        private void LoadPosNegWords()
+        {
+            var positiveWords =
+                File.ReadLines(@"Resources\positive-words.txt")
+                    .Where(s => !s.StartsWith(";") && !string.IsNullOrWhiteSpace(s));
+
+            foreach (var word in positiveWords)
+            {
+                this.AddSentiment(word, 2.5F);
+            }
+
+            var negativeWords =
+                File.ReadLines(@"Resources\negative-words.txt")
+                    .Where(s => !s.StartsWith(";") && !string.IsNullOrWhiteSpace(s));
+
+            foreach (var word in negativeWords)
+            {
+                this.AddSentiment(word, -2.5F);
+            }
+        }
+
+        private void LoadAffin()
+        {
+            var wordScores = from line in File.ReadLines(@"Resources\AFINN-111.txt")
+                             let parts = line.Split('\t')
+                             select new { Word = parts[0], Score = float.Parse(parts[1]) };
+
+            foreach (var wordScore in wordScores)
+            {
+                this.AddSentiment(wordScore.Word, wordScore.Score);
+            }
         }
 
         private void AddSentiment(string word, float score)
