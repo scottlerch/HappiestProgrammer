2240bf66a9d8eeb9bfc7f148074a05a3a874094d
Scott Lerch <scottlerch@gmail.com>
2014-01-18 18:37:31 -0800

    Hook up basic display of language scores using AngularJS.

--- WebApp/App_Start/BundleConfig.cs
+++ WebApp/App_Start/BundleConfig.cs
@@ -18,6 +18,15 @@ namespace WebApp
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                        "~/Scripts/app.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/toastr").Include(
+                        "~/Scripts/toastr.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ WebApp/Scripts/app.js
@@ -0,0 +1,43 @@
+﻿var app = angular.module("app", []);
+
+var url = 'api/languages?date=2014-01-13';
+
+app.factory('languageFactory', function ($http) {
+    return {
+        getLanguages: function () {
+            return $http.get(url);
+        },
+    };
+});
+
+app.factory('notificationFactory', function () {
+
+    return {
+        success: function () {
+            toastr.success("Success");
+        },
+        error: function (text) {
+            toastr.error(text, "Error!");
+        }
+    };
+});
+
+app.controller("HomeCtrl", function ($scope, languageFactory, notificationFactory) {
+    $scope.languages = [];
+
+    var getLanguagesSuccessCallback = function (data, status) {
+        $scope.languages = data;
+    };
+
+    var successCallback = function (data, status, headers, config) {
+        notificationFactory.success();
+
+        return languageFactory.getLanguages().success(getLanguagesSuccessCallback).error(errorCallback);
+    };
+
+    var errorCallback = function (data, status, headers, config) {
+        notificationFactory.error(data.ExceptionMessage);
+    };
+
+    languageFactory.getLanguages().success(getLanguagesSuccessCallback).error(errorCallback);
+});
