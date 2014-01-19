2a9f2b1ecb5264537b55def89f962c47320e1702
Scott Lerch <scottlerch@gmail.com>
2014-01-18 17:35:40 -0800

    Clean up website skeleton.

--- WebApp/Global.asax.cs
+++ WebApp/Global.asax.cs
@@ -9,15 +9,10 @@ using System.Web.Routing;
 
 namespace WebApp
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
     public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
